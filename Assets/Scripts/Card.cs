d0adb9b3aa9f0d4b327329338c68516ae8988c15
SannanZahid <103260112+SannanZahid@users.noreply.github.com>
2023-08-19 00:03:58 +0500

    Card Flipping animation on mismatch implemented
    Implemented card flipping animation on mismatching cards in parallel to main thread using coroutine with SmoothDampAngle for rotation of cards after validation.

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -12,6 +13,7 @@ public class Card : MonoBehaviour
     private Button _cardBtn; //Refernce to button for card interation toggle
     public enum CardSides { Front, Back } //State of the card
     Action<Card> _callbackGameBoard; //callback for card selected
+    float _cardRotationVelocity;// used for SmoothDampAngle to achieve card flipping animation functionality
     //Initialize the card front with sprite, adds button and binds interation listener to button
     public void Init(int cardId, Sprite cardFace, Action<Card> callback)
     {
@@ -29,14 +31,12 @@ public class Card : MonoBehaviour
         {
             case CardSides.Front:
                 {
-                    _cardFront.gameObject.SetActive(true);
-                    _cardBack.gameObject.SetActive(false);
+                    StartCoroutine(CardAnimationRotationCoroutine(_cardBack, _cardFront));
                     break;
                 }
             case CardSides.Back:
                 {
-                    _cardFront.gameObject.SetActive(false);
-                    _cardBack.gameObject.SetActive(true);
+                    StartCoroutine(CardAnimationRotationCoroutine(_cardFront, _cardBack));
                     break;
                 }
         }
@@ -60,5 +60,38 @@ public class Card : MonoBehaviour
         _cardFront.gameObject.SetActive(false);
         _cardBack.gameObject.SetActive(false);
         _cardBtn.interactable = false;
+    }
+    IEnumerator CardAnimationRotationCoroutine(Transform cardF, Transform cardB)
+    {
+        cardB.gameObject.SetActive(false);
+        cardF.gameObject.SetActive(true);
+        cardF.transform.rotation = Quaternion.Euler(new Vector3(0.0f, 0.0f, 0.0f));
+        cardB.transform.rotation = Quaternion.Euler(new Vector3(0.0f, 90.0f, 0.0f));
+        while (true)
+        {
+            float Angle = Mathf.SmoothDampAngle(cardF.eulerAngles.y, 90f, ref _cardRotationVelocity, 0.05f);
+            cardF.rotation = Quaternion.Euler(0, Angle, 0);
+            if (cardF.eulerAngles.y >= 89.0f)
+            {
+                cardF.gameObject.SetActive(false);
+                cardB.gameObject.SetActive(true);
+                break;
+            }
+            else
+                yield return null;
+        }
+        while (true)
+        {
+            float Angle = Mathf.SmoothDampAngle(cardB.eulerAngles.y, 0f, ref _cardRotationVelocity, 0.05f);
+            cardB.rotation = Quaternion.Euler(0, Angle, 0);
+            if (cardB.eulerAngles.y <= 0.1f)
+            {
+                break;
+            }
+            else
+                yield return null;
+        }
+        cardF.transform.rotation = Quaternion.Euler(new Vector3(0.0f, 0.0f, 0.0f));
+        cardB.transform.rotation = Quaternion.Euler(new Vector3(0.0f, 0.0f, 0.0f));
     }
 }
