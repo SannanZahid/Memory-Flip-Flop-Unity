02c04b67274df2c75ce7e2c6c231a867cad9196a
SannanZahid <103260112+SannanZahid@users.noreply.github.com>
2023-08-19 03:39:44 +0500

    Level progress and Score Board Implement to Main Menu
    Player progress though out the game is store and is displayed also on main menu to give user sense of progress, when player comes to main menu on game open or on scene switch.

--- Assets/Scripts/Managers/MainMenuManager.cs
+++ Assets/Scripts/Managers/MainMenuManager.cs
@@ -3,7 +3,9 @@ using UnityEngine;
 
 public class MainMenuManager : MonoBehaviour
 {
- 
+    [Header("Pass Text References")]
+    [SerializeField]
+    private TMP_Text _totalMatches, _totalTurns, _totalCombo, _gameLevel;
 
     //In start if first time user enters default playerpref are set
     //in the GameConstantsPlayerPref class
@@ -13,7 +15,30 @@ public class MainMenuManager : MonoBehaviour
         {
             GameConstantsPlayerPref.SetDefaultGameValues();
         }
+        SetMenuScoreBoard();
     }
-
+    void SetMenuScoreBoard()
+    {
+        SetTotalComboText("" + GameConstantsPlayerPref.GetTotalCombo());
+        SetTotalMatchesText("" + GameConstantsPlayerPref.GetTotalMatches());
+        SetTotalTurnsText("" + GameConstantsPlayerPref.GetTotalTurns());
+        SetGameLevelText("LEVEL " + GameConstantsPlayerPref.GetGameLevel());
+    }
+    public void SetTotalComboText(string messageText)
+    {
+        _totalCombo.text = messageText;
+    }
+    public void SetTotalMatchesText(string messageText)
+    {
+        _totalMatches.text = messageText;
+    }
+    public void SetTotalTurnsText(string messageText)
+    {
+        _totalTurns.text = messageText;
+    }
+    public void SetGameLevelText(string messageText)
+    {
+        _gameLevel.text = messageText;
+    }
 
 }
--- Assets/Scripts/TimeSystem.cs
+++ Assets/Scripts/TimeSystem.cs
@@ -5,19 +5,24 @@ public class TimeSystem : MonoBehaviour
     private float _defaultTime = default;
     public float TimeRemaining = 10;
     private bool _timerIsRunning = false;
+    // In start original time is stored 
+    // later if you want to reset time on level fail / complete
     public void Start()
     {
         _defaultTime = TimeRemaining;
     }
+    // funcation is called when games starts to keep playtime record
     public void StartGameTimer()
     {
         _timerIsRunning = true;
     }
+    //reset time one level is complete or fail
     public void ResetTimer()
     {
         TimeRemaining = _defaultTime;
         _timerIsRunning = true;
     }
+    //stops timer in case of level complete so level fail is not called
     public void StopLevelTimer()
     {
         _timerIsRunning = false;
